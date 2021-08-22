f205d78a343703dcff6886ab475e3805a6e9b930
MCUheart <38098912+MCUheart@users.noreply.github.com>
2021-08-23 00:02:04 +0800

    更新gitignore

--- Blog_Data_AutoPush.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-git add .
-git commit -m "WIN10更新图床"
-git tag -d 1.0.2
-git push origin --delete tag 1.0.2
-git tag -a 1.0.2 -m "V1.0.2图床版本"
-git push origin master
-git push origin 1.0.2
